4abb9f27b37aad8866318fe6a1d3d265e5e3d62c
tranvinh146 <vinh.tt@geekup.vn>
2022-07-01 14:10:51 +0700

    add cors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+const cors =require("cors")
 const port = process.env.PORT || 3000;
 
+app.use(cors())
 app.use(express.json());
 
 app.get("/", (req, res) => {
